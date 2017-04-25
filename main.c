baa702b00d61116fd65b89cbd62bc9eee07fb86e
MrWISKI <VTV1994@Mail.ru>
2017-04-26 00:34:40 +0700

    Add input validation

--- main.c
+++ main.c
@@ -4,7 +4,15 @@ int main () {
     int money, day;
     printf ("Specify the Deposit amount (RUR): ");
     scanf ("%d", money);
+    while (maney<10000) {
+        printf ("Invalid Deposit amount! Enter again: ");
+        scanf ("%d", money);
+    } 
     printf ("Specify the Deposit term (days): ");
+    while (day>365) {
+        printf ("Wrong term Deposit! Enter again: ");
+        scanf ("%d", day);
+    }
     scanf ("%d", day);
     return 0;
 }
