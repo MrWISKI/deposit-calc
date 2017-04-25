10775adeaeae55c9daaf9dd244cbd658e267ec2b
MrWISKI <VTV1994@Mail.ru>
2017-04-26 00:27:44 +0700

    Add input

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main () {
+    int money, day;
+    printf ("Specify the Deposit amount (RUR): ");
+    scanf ("%d", money);
+    printf ("Specify the Deposit term (days): ");
+    scanf ("%d", day);
     return 0;
 }
