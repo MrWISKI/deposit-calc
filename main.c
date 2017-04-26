e3b6f2a7973d688ed22e6919d343557e39d4b434
MrWISKI <VTV1994@Mail.ru>
2017-04-26 19:06:05 +0700

    Add yield

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 
 int main () {
     int day;
-    float money;
+    float money, money1;
     printf ("Specify the Deposit amount (RUR): ");
     scanf ("%f", &money);
     while (money<10000.0) {
         printf ("Invalid Deposit amount! Enter again: ");
         scanf ("%f", &money);
-    } 
+    }
+    money1=money;
     printf ("Specify the Deposit term (days): ");
     scanf ("%d", &day);
     while (day>365 || day<0) {
@@ -19,26 +20,27 @@ int main () {
         if (day<241) {
             if (day<121) {
                 if (day<31) {
-                    money-=money*day*0.1/365;
+                    money1-=money*day*0.1/365;
                 }
-                else money+=money*day*0.02/365;
+                else money1+=money*day*0.02/365;
             }
-            else money+=money*day*0.06/365;
+            else money1+=money*day*0.06/365;
         }
-        else money+=money*day*0.12/365;
+        else money1+=money*day*0.12/365;
     }
     else {
         if (day<241) {
             if (day<121) {
                 if (day<31) {
-                    money-=money*day*0.1/365;
+                    money1-=money*day*0.1/365;
                 }
-                else money+=money*day*0.03/365;
+                else money1+=money*day*0.03/365;
             }
-            else money+=money*day*0.08/365;
+            else money1+=money*day*0.08/365;
         }
-        else money+=money*day*0.15/365;
+        else money1+=money*day*0.15/365;
     }
-    printf ("The Deposit amount at the end of the period: %.2f!\n", money);
+    printf ("The Deposit amount at the end of the period: %.2f!\n", money1);
+    printf ("The contribution of yield: %.2f!\n", money1-money);
     return 0;
 }
